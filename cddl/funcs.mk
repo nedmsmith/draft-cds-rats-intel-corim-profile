69dc64c11faa3987a820eee843ff59489993302d
nedmsmith <ned.smith@intel.com>
2025-01-08 08:54:34 -0800

    added support for imports and exports
    added imports dir containing corim and concise-evidence cddl files rather than rely on submodules.

---
+++ cddl/funcs.mk
@@ -0,0 +1,60 @@
+# $1: label
+# $2: cddl fragments
+define cddl_autogen_template
+check-profile: $(1)-autogen.cddl
+
+$(1)-autogen.cddl: $(2)
+	for f in $$^ ; do ( grep -v '^;' $$$$f ; echo ) ; done > $$@
+
+CLEANFILES += $(1)-autogen.cddl
+endef # cddl_autogen_template
+
+# $1: label
+# $2: cddl fragments
+# $3: diag test files
+define cddl_check_template
+
+check-$(1): $(1)-autogen.cddl
+	$$(cddl) $$< g 1 | $$(diag2diag) -e
+
+.PHONY: check-$(1)
+
+$(1)-autogen.cddl: $(2)
+	for f in $$^ ; do ( grep -v '^;' $$$$f ; echo ) ; done > $$@
+
+CLEANFILES += $(1)-autogen.cddl
+
+check-$(1)-examples: $(1)-autogen.cddl $(3:.diag=.cbor)
+	@for f in $(3:.diag=.cbor); do \
+		echo ">> validating $$$$f against $$<" ; \
+		$$(cddl) $$< validate $$$$f &>/dev/null || exit 1 ; \
+		echo ">> saving prettified CBOR to $$$${f%.cbor}.pretty" ; \
+		$$(cbor2pretty) $$$$f > $$$${f%.cbor}.pretty ; \
+	done
+
+.PHONY: check-$(1)-examples
+
+CLEANFILES += $(3:.diag=.cbor)
+CLEANFILES += $(3:.diag=.pretty)
+
+endef # cddl_check_template
+
+# $(1) - export label
+# $(2) - cddl fragments 
+# $(3) - export directory
+define cddl_exp_template
+
+exp-$(1): $(3)$(1).cddl
+	echo ">>> Creating exportable cddl file" ;
+
+.PHONY: exp-$(1)
+
+$(EXPORTS_DIR)$(1).cddl: $(2)
+	
+	@for f in $$^ ; do \
+		( grep -v '^;' $$$$f ; echo ) ; \
+	done > $$@
+
+CLEANFILES += $(3)$(1).cddl
+
+endef # cddl_exp_template
